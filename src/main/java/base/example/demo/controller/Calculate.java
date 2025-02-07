fd68fe1049347c6d6feaa69596a6f4c5f1decc9b
Sergey Rogozhkin <r8damik@gmail.com>
2025-02-07 18:20:08 +0300

    commit 07.02

---
+++ src/main/java/base/example/demo/controller/Calculate.java
@@ -0,0 +1,15 @@
+package base.example.demo.controller;
+
+public class Calculate {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public boolean eveOrOdd(int number){
+        return number % 2 == 0;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
